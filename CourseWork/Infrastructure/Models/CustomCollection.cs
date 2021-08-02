d85724d3461c65adf2ec11f9b954708783ae1f50
Милюк Вадим <milyuk.vadim1@mail.ru>
2021-08-02 21:13:33 +0300

    Added models

---
+++ CourseWork/Infrastructure/Models/CustomCollection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace CourseWork.Infrastructure.Models
+{
+    public class CustomCollection
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Topic { get; set; }
+        public string Img { get; set; }
+        public bool IsNumericField1Visible { get; set; }
+        public bool IsNumericField2Visible { get; set; }
+        public bool IsNumericField3Visible { get; set; }
+        public bool IsOneLineField1Visible { get; set; }
+        public bool IsOneLineField2Visible { get; set; }
+        public bool IsOneLineField3Visible { get; set; }
+        public bool IsTextField1Visible { get; set; }
+        public bool IsTextField2Visible { get; set; }
+        public bool IsTextField3Visible { get; set; }
+        public bool IsDate1Visible { get; set; }
+        public bool IsDate2Visible { get; set; }
+        public bool IsDate3Visible { get; set; }
+        public bool IsCheckBox1Visible { get; set; }
+        public bool IsCheckBox2Visible { get; set; }
+        public bool IsCheckBox3Visible { get; set; }
+        public ICollection<Item> Items { get; set; }
+    }
+}

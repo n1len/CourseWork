fdbda851d42e9af8e9f1c1d48f2ae2907c14d8f3
Милюк Вадим <milyuk.vadim1@mail.ru>
2021-08-07 16:26:08 +0300

    Bind the visibility of the fields in the Item to the checkbox in the CustomCollection, changed the Create, Details and Edit views

--- CourseWork/Infrastructure/Models/Item.cs
+++ CourseWork/Infrastructure/Models/Item.cs
@@ -1,25 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseWork.Infrastructure.Models
 {
     public class Item
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Название айтема не может быть пустым")]
+        [Display(Name = "Название айтема")]
         public string Title { get; set; }
+
+        [Display(Name = "Тэги")]
         public string Tags { get; set; }
-        public int NumericField1 { get; set; }
-        public int NumericField2 { get; set; }
-        public int NumericField3 { get; set; }
+
+        [RegularExpression(@"^[0-9\s,.]{0,300}")]
+        public string NumericField1 { get; set; }
+
+        [RegularExpression(@"^[0-9\s,.]{0,300}")]
+        public string NumericField2 { get; set; }
+
+        [RegularExpression(@"^[0-9\s,.]{0,300}")]
+        public string NumericField3 { get; set; }
+
         public string OneLineField1 { get; set; }
         public string OneLineField2 { get; set; }
         public string OneLineField3 { get; set; }
         public string TextField1 { get; set; }
         public string TextField2 { get; set; }
         public string TextField3 { get; set; }
-        public DateTime Date1 { get; set; }
-        public DateTime Date2 { get; set; }
-        public DateTime Date3 { get; set; }
+
+        [DataType(DataType.Date)]
+        public string Date1 { get; set; }
+
+        [DataType(DataType.Date)]
+        public string Date2 { get; set; }
+
+        [DataType(DataType.Date)]
+        public string Date3 { get; set; }
+
         public bool CheckBox1 { get; set; }
         public bool CheckBox2 { get; set; }
         public bool CheckBox3 { get; set; }

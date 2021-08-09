871c210630260f44a5ea1c701d957b5964687eed
Милюк Вадим <milyuk.vadim1@mail.ru>
2021-08-09 17:02:56 +0300

    Implemented the main page with the last added items, a collection with the largest number of items, a tag cloud

---
+++ CourseWork/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseWork.Infrastructure.Models;
+
+namespace CourseWork.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<CustomCollection> CustomCollections { get; set; }
+        public IEnumerable<Item> Items { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+    }
+}

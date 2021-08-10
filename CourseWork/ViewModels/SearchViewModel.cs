fce83ce606d7fa9b162697ad42671b1a28f09eb4
Милюк Вадим <milyuk.vadim1@mail.ru>
2021-08-10 12:50:29 +0300

    Implemented full text search

---
+++ CourseWork/ViewModels/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseWork.Infrastructure.Models;
+
+namespace CourseWork.ViewModels
+{
+    public class SearchViewModel
+    {
+        public IEnumerable<Item> Items { get; set; }
+        public IEnumerable<Comment> Comments { get; set; }
+        public string Query { get; set; }
+    }
+}

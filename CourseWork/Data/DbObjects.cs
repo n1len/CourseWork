8b57afe9361996d6daa4ac58e4dae8b622a4d617
Милюк Вадим <milyuk.vadim1@mail.ru>
2021-08-13 15:55:45 +0300

    Implemented tags using tagify, no autocompletion

--- CourseWork/Data/DbObjects.cs
+++ CourseWork/Data/DbObjects.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CourseWork.Infrastructure.Models;
+using CourseWork.ViewModels;
 using Microsoft.EntityFrameworkCore;
 
 namespace CourseWork.Data
